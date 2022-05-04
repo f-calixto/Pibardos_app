f89c43121ee5f5f8027af93a7193178547018bb6
Agustín Lanusse <lanusseagustin@gmail.com>
2022-05-03 21:06:06 -0300

    modify eslint rules

--- nodejs/apps/mobile/App.js
+++ nodejs/apps/mobile/App.js
@@ -1,4 +1,3 @@
-import React from 'react'
 // 1. import `NativeBaseProvider` component
 import { NativeBaseProvider, Text, Box } from 'native-base'
 import { StatusBar } from 'expo-status-bar'
@@ -7,7 +6,7 @@ export default function App () {
   // 2. Use at the root of your app
   return (
     <NativeBaseProvider>
-      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+      <Box flex={1} bg='#fff' alignItems='center' justifyContent='center'>
         <Text>Open up App.js to start working on your app!</Text>
       </Box>
       <StatusBar style='dark' />
