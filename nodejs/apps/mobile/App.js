707fae302ba9419bd26a70bd80dc659709203f9b
Agustín Lanusse <lanusseagustin@gmail.com>
2022-05-16 20:29:52 -0300

    splash screen

--- nodejs/apps/mobile/App.js
+++ nodejs/apps/mobile/App.js
@@ -1,15 +1,11 @@
-// 1. import `NativeBaseProvider` component
 import { NativeBaseProvider } from 'native-base'
-import SplashScreen from './src/containers/SplashScreen'
+// import SplashScreen from './src/containers/SplashScreen'
+import AuthScreen from './src/screens/AuthScreen'
 
 export default function App () {
-  // 2. Use at the root of your app
   return (
     <NativeBaseProvider>
-        <SplashScreen />
-      {/* <Box flex={1} bg='#fff' alignItems='center' justifyContent='center'>
-        <Text>Open up App.js to start working on your app!</Text>
-      </Box> */}
+      <AuthScreen />
     </NativeBaseProvider>
   )
 }
