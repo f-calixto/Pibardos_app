5c83a89d895295f5e91bf1b5c488be20f04d7045
Agustín Lanusse <lanusseagustin@gmail.com>
2022-06-02 11:41:52 -0300

    refactor app.js

--- nodejs/apps/authentication-service/app.js
+++ nodejs/apps/authentication-service/app.js
@@ -1,8 +1,19 @@
 const express = require('express')
-const app = express()
 const logger = require('morgan')
+const errorMiddleware = require('./middlewares/error')
 
+// init app
+const app = express()
+
+// Middlewares
 app.use(express.json())
 app.use(logger('dev'))
+
+// Routes
+// app.use('/token', require('./routes/token.routes')) // token routes
+app.use('/', require('./routes/users.routes')) // users routes
+
+// Errors middleware
+app.use(errorMiddleware)
 
 module.exports = app
