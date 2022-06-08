17e4f9694d3917f86c43d8b73dd1d321dc0d8f0f
Agustín Lanusse <lanusseagustin@gmail.com>
2022-06-08 10:22:17 -0300

    format code

--- nodejs/apps/mobile/src/screens/LoginScreen.jsx
+++ nodejs/apps/mobile/src/screens/LoginScreen.jsx
@@ -8,32 +8,46 @@ const LoginScreen = () => {
   const [showPswd, setShowPswd] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
   return (
     <Flex flex='1'>
-
-    <Flex justify='center' align='center' mt='50%'>
+      <Flex justify='center' align='center' mt='50%'>
         <Image
           source={require('../assets/logos/favicon.png') }
           width='50%'
           height= '50%'
-          alt='logo'>
-
-        </Image>
-      <Text mt='3' fontSize='35' fontWeight='bold'>Iniciar Sesion</Text>
-    </Flex>
-    <Flex mt='10%' justify='space-between' align='center' >
-        <Input width='90%' placeholder='E-mail o nombre de usuario' onChangeText={email => setEmail(email)}></Input>
-        <Input width='90%' placeholder='Contraseña' onChangeText={pswd => setPassword(pswd)} type={showPswd ? 'text' : 'password'} InputRightElement={<Icon as={<MaterialIcons name={showPswd ? 'visibility' : 'visibility-off'} />} size={5} mr='2' color='muted.400' onPress={() => setShowPswd(!showPswd)} />}></Input>
-        <Button width='70%' bg={theme.colors.blue} height='20%' onPress={() => console.log('Email: ' + email + ' Pswd: ' + password)}>
-        Iniciar Sesion
-      </Button>
-      <Pressable onPress={() => console.log('Olvide contrasena')}>
-          <Text>Olvide mi contraseña</Text>
-      </Pressable>
-      <Pressable onPress={() => console.log('No estoy registrado')}>
-          <Text>No estoy registrado</Text>
-      </Pressable>
-    </Flex>
+          alt='logo'
+        />
+        <Text mt='3' fontSize='35' fontWeight='bold'>Iniciar Sesion</Text>
+      </Flex>
+      <Flex mt='10%' justify='space-between' align='center' >
+        <Input width='90%' placeholder='E-mail o nombre de usuario' onChangeText={email => setEmail(email)} />
+        <Input
+          width='90%'
+          placeholder='Contraseña'
+          onChangeText={pswd => setPassword(pswd)}
+          type={showPswd ? 'text' : 'password'}
+          InputRightElement={<Icon as={<MaterialIcons name={showPswd ? 'visibility' : 'visibility-off'} />}
+          size={5}
+          mr='2'
+          color='muted.400'
+          onPress={() => setShowPswd(!showPswd)} />}
+        />
+        <Button
+          width='70%'
+          bg={theme.colors.blue}
+          height='20%'
+          onPress={() => console.log('Email: ' + email + ' Pswd: ' + password)}
+        >
+          Iniciar Sesion
+        </Button>
+        <Pressable onPress={() => console.log('Olvide contrasena')}>
+            <Text>Olvide mi contraseña</Text>
+        </Pressable>
+        <Pressable onPress={() => console.log('No estoy registrado')}>
+            <Text>No estoy registrado</Text>
+        </Pressable>
+      </Flex>
     </Flex>
   )
 }
--- nodejs/apps/mobile/src/screens/RegisterScreen.jsx
+++ nodejs/apps/mobile/src/screens/RegisterScreen.jsx
@@ -12,34 +12,32 @@ const LoginScreen = () => {
   const [userName, setUserName] = useState('')
   const [birthday, setBirthday] = useState('')
   const [country, setCountry] = useState('')
+
   return (
-
     <Flex flex='1'>
-
-    <Flex justify='center' align='center' mt='30%'>
+      <Flex justify='center' align='center' mt='30%'>
         <Image
           source={require('../assets/logos/favicon.png') }
           width='50%'
           height= '50%'
-          alt='logo'>
-
-        </Image>
-      <Text mt='3' fontSize='35' fontWeight='bold'>Registrar Cuenta</Text>
-    </Flex>
-    <Flex mt='10%' justify='space-between' align='center' >
+          alt='logo'
+        />
+        <Text mt='3' fontSize='35' fontWeight='bold'>Registrar Cuenta</Text>
+      </Flex>
+      <Flex mt='10%' justify='space-between' align='center' >
         <Input width='90%' keyboardType='email-address' placeholder='E-mail' onChangeText={email => setEmail(email)}></Input>
         <Input width='90%' type={showPswd ? 'text' : 'password'}
           InputRightElement={<Icon as={<MaterialIcons name={showPswd ? 'visibility' : 'visibility-off'} />}
           size={5} mr='2' color='muted.400' onPress={() => setShowPswd(!showPswd)} />}
           placeholder='Contraseña'
-          onChangeText={pswd => setPassword(pswd)}>
-        </Input>
+          onChangeText={pswd => setPassword(pswd)}
+        />
         <Input width='90%' type={showPswd ? 'text' : 'password'}
           InputRightElement={<Icon as={<MaterialIcons name={showPswd ? 'visibility' : 'visibility-off'} />}
           size={5} mr='2' color='muted.400' onPress={() => setShowPswd(!showPswd)} />}
           placeholder='Repetir contraseña'
-          onChangeText={verifyPswd => setVerifyPassword(verifyPswd)}>
-        </Input>
+          onChangeText={verifyPswd => setVerifyPassword(verifyPswd)}
+        />
         <Input width='90%' placeholder='Nombre de usuario' onChangeText={user => setUserName(user)}></Input>
         <Input width='90%' placeholder='Cumpleanos' onChangeText={bday => setBirthday(bday)}></Input>
         <Select minWidth='90%' accessibilityLabel='Pais' placeholder='Pais' onValueChange={(country) => setCountry(country) }>
@@ -52,18 +50,18 @@ const LoginScreen = () => {
           <Select.Item label='Inglaterra' value='UK' />
           <Select.Item label='Brasil' value='BR' />
         </Select>
-        <Button width='70%' bg={theme.colors.green} height='12%' mt='5%' onPress={() => console.log('email: ' + email + '\n' +
-                                                                                                    'password: ' + password + '\n' +
-                                                                                                    'verifyPassword: ' + verifyPassword + '\n' +
-                                                                                                    'username: ' + userName + '\n' +
-                                                                                                    'bday: ' + birthday + '\n' +
-                                                                                                    'country: ' + country + '\n')}>
-        Registrarse
-      </Button>
-      <Pressable onPress={() => console.log('Ya tengo una cuenta')}>
+        <Button
+          width='70%'
+          bg={theme.colors.green}
+          height='12%'
+          mt='5%'
+          onPress={() => console.log({ email, password, verifyPassword, userName, birthday, country })}>
+          Registrarse
+        </Button>
+        <Pressable onPress={() => console.log('Ya tengo una cuenta')}>
           <Text>Ya tengo una cuenta</Text>
-      </Pressable>
-    </Flex>
+        </Pressable>
+      </Flex>
     </Flex>
   )
 }
