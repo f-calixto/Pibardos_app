6aacaad0c8ac8025175963f0a67cd1146f49100d
Ivan Czar <ivanczar2013@gmail.com>
2022-06-08 17:37:30 +1200

    implemented show/hide pswd and useStates on login/register screen

--- nodejs/apps/mobile/src/screens/LoginScreen.jsx
+++ nodejs/apps/mobile/src/screens/LoginScreen.jsx
@@ -1,8 +1,13 @@
-import { Button, Input, Flex, Text, Pressable } from 'native-base'
+import { Button, Input, Flex, Text, Pressable, Icon } from 'native-base'
 import { Image } from 'react-native'
 import theme from '../../theme'
+import React, { useState } from 'react'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 const LoginScreen = () => {
+  const [showPswd, setShowPswd] = React.useState(false)
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
   return (
     <Flex flex='1'>
 
@@ -17,9 +22,9 @@ const LoginScreen = () => {
       <Text mt='3' fontSize='35' fontWeight='bold'>Iniciar Sesion</Text>
     </Flex>
     <Flex mt='10%' justify='space-between' align='center' >
-        <Input width='90%' placeholder='E-mail o nombre de usuario'></Input>
-        <Input width='90%' placeholder='Contraseña'></Input>
-        <Button width='70%' bg={theme.colors.blue} height='20%' onPress={() => console.log('login press')}>
+        <Input width='90%' placeholder='E-mail o nombre de usuario' onChangeText={email => setEmail(email)}></Input>
+        <Input width='90%' placeholder='Contraseña' onChangeText={pswd => setPassword(pswd)} type={showPswd ? 'text' : 'password'} InputRightElement={<Icon as={<MaterialIcons name={showPswd ? 'visibility' : 'visibility-off'} />} size={5} mr='2' color='muted.400' onPress={() => setShowPswd(!showPswd)} />}></Input>
+        <Button width='70%' bg={theme.colors.blue} height='20%' onPress={() => console.log('Email: ' + email + ' Pswd: ' + password)}>
         Iniciar Sesion
       </Button>
       <Pressable onPress={() => console.log('Olvide contrasena')}>
--- nodejs/apps/mobile/src/screens/RegisterScreen.jsx
+++ nodejs/apps/mobile/src/screens/RegisterScreen.jsx
@@ -1,8 +1,17 @@
-import { Button, Input, Flex, Text, Pressable, Select } from 'native-base'
+import { Button, Input, Flex, Text, Pressable, Select, Icon } from 'native-base'
 import { Image } from 'react-native'
 import theme from '../../theme'
+import React, { useState } from 'react'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 const LoginScreen = () => {
+  const [showPswd, setShowPswd] = React.useState(false)
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [verifyPassword, setVerifyPassword] = React.useState('')
+  const [userName, setUserName] = React.useState('')
+  const [birthday, setBirthday] = React.useState('')
+  const [country, setCountry] = React.useState('')
   return (
 
     <Flex flex='1'>
@@ -18,12 +27,22 @@ const LoginScreen = () => {
       <Text mt='3' fontSize='35' fontWeight='bold'>Registrar Cuenta</Text>
     </Flex>
     <Flex mt='10%' justify='space-between' align='center' >
-        <Input width='90%' placeholder='E-mail'></Input>
-        <Input width='90%' placeholder='Contraseña'></Input>
-        <Input width='90%' placeholder='Repetir contraseña'></Input>
-        <Input width='90%' placeholder='Nombre de usuario'></Input>
-        <Input width='90%' placeholder='Cumpleanos'></Input>
-        <Select minWidth='90%' accessibilityLabel='Pais' placeholder='Pais' onValueChange={(value) => console.log('Country changed: ' + value)}>
+        <Input width='90%' keyboardType='email-address' placeholder='E-mail' onChangeText={email => setEmail(email)}></Input>
+        <Input width='90%' type={showPswd ? 'text' : 'password'}
+          InputRightElement={<Icon as={<MaterialIcons name={showPswd ? 'visibility' : 'visibility-off'} />}
+          size={5} mr='2' color='muted.400' onPress={() => setShowPswd(!showPswd)} />}
+          placeholder='Contraseña'
+          onChangeText={pswd => setPassword(pswd)}>
+        </Input>
+        <Input width='90%' type={showPswd ? 'text' : 'password'}
+          InputRightElement={<Icon as={<MaterialIcons name={showPswd ? 'visibility' : 'visibility-off'} />}
+          size={5} mr='2' color='muted.400' onPress={() => setShowPswd(!showPswd)} />}
+          placeholder='Repetir contraseña'
+          onChangeText={verifyPswd => setVerifyPassword(verifyPswd)}>
+        </Input>
+        <Input width='90%' placeholder='Nombre de usuario' onChangeText={user => setUserName(user)}></Input>
+        <Input width='90%' placeholder='Cumpleanos' onChangeText={bday => setBirthday(bday)}></Input>
+        <Select minWidth='90%' accessibilityLabel='Pais' placeholder='Pais' onValueChange={(country) => setCountry(country) }>
           <Select.Item label='Argentina' value='AR' />
           <Select.Item label='Uruguay' value='UY' />
           <Select.Item label='Nueva Zelandia' value='NZ' />
@@ -33,7 +52,12 @@ const LoginScreen = () => {
           <Select.Item label='Inglaterra' value='UK' />
           <Select.Item label='Brasil' value='BR' />
         </Select>
-        <Button width='70%' bg={theme.colors.green} height='12%' mt='5%' onPress={() => console.log('register press')}>
+        <Button width='70%' bg={theme.colors.green} height='12%' mt='5%' onPress={() => console.log('email: ' + email + '\n' +
+                                                                                                    'password: ' + password + '\n' +
+                                                                                                    'verifyPassword: ' + verifyPassword + '\n' +
+                                                                                                    'username: ' + userName + '\n' +
+                                                                                                    'bday: ' + birthday + '\n' +
+                                                                                                    'country: ' + country + '\n')}>
         Registrarse
       </Button>
       <Pressable onPress={() => console.log('Ya tengo una cuenta')}>
