8004e07f5683f5fc8e51ab90d4ef144ad9a2a2cb
Agustín Lanusse <lanusseagustin@gmail.com>
2022-06-07 22:58:00 -0300

    add theme file

---
+++ nodejs/apps/mobile/theme.js
@@ -0,0 +1,26 @@
+export default {
+  colors: {
+    primary: '#0366d6',
+    white: '#fff',
+    grey: '#666',
+    red: '#d73a4a',
+    lightGrey: '#e1e4e8',
+    transparentBlack: '#24292e'
+  },
+  fonts: {
+    default: 'System',
+    android: 'Roboto',
+    ios: 'Arial'
+  },
+  fontSizes: {
+    large: 22,
+    medium: 18,
+    small: 15
+  },
+  fontWeights: {
+    bold: '700'
+  },
+  imageSizes: {
+    medium: 65
+  }
+}
