c8b5f964772e90ea5d3413cfa07264a6257cfe19
Agustín Lanusse <lanusseagustin@gmail.com>
2022-06-24 13:25:36 -0300

    add babel module-resolvers

--- nodejs/apps/mobile/babel.config.js
+++ nodejs/apps/mobile/babel.config.js
@@ -1,6 +1,27 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        alias: {
+          '@Components': './src/components',
+          '@Containers': './src/containers',
+          '@Screens': './src/screens',
+          '@ReduxSlices': './src/redux',
+          '@Hooks': './src/hooks',
+          '@Assets': './src/assets',
+          '@Theme': './theme.js',
+          '@Utils': './src/utils',
+          '@Services': './src/services'
+        },
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx'
+        ]
+      }]
+    ]
   }
 }
