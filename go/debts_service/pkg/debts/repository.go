34ee5fcdbad2bf9fbd95cb4dd57146a2978b2366
coding-kiko <kikoaudi2001@gmail.com>
2022-07-19 18:07:31 -0300

    w

--- go/debts_service/pkg/debts/repository.go
+++ go/debts_service/pkg/debts/repository.go
@@ -52,11 +52,11 @@ func (r *repo) CreateDebt(debt Debt) error {
 
 func (r *repo) AcceptDebt(req PatchDebtRequest) (Debt, error) {
 	rows, err := r.db.Exec(patchDebtQuery, 1, req.DebtId, req.UserId)
-	if err != nil {
-		return Debt{}, errors.NewNotFound("debt not found")
-	}
 	if n, _ := rows.RowsAffected(); n == 0 {
 		return Debt{}, errors.NewUnauthorized("User accepting is not the same as the borrower")
+	}
+	if err != nil {
+		return Debt{}, errors.NewNotFound("debt not found")
 	}
 	updatedDebt, err := r.GetDebt(req.DebtId)
 	if err != nil {
@@ -67,11 +67,11 @@ func (r *repo) AcceptDebt(req PatchDebtRequest) (Debt, error) {
 
 func (r *repo) RejectDebt(req PatchDebtRequest) (Debt, error) {
 	rows, err := r.db.Exec(patchDebtQuery, 0, req.DebtId, req.UserId)
-	if err != nil {
-		return Debt{}, errors.NewNotFound("debt not found")
-	}
 	if n, _ := rows.RowsAffected(); n == 0 {
 		return Debt{}, errors.NewUnauthorized("User accepting is not the same as the borrower")
+	}
+	if err != nil {
+		return Debt{}, errors.NewNotFound("debt not found")
 	}
 	updatedDebt, err := r.GetDebt(req.DebtId)
 	if err != nil {
@@ -82,11 +82,11 @@ func (r *repo) RejectDebt(req PatchDebtRequest) (Debt, error) {
 
 func (r *repo) CancelDebt(req PatchDebtRequest) (Debt, error) {
 	rows, err := r.db.Exec(cancelDebtQuery, 3, req.DebtId, req.UserId)
-	if err != nil {
-		return Debt{}, errors.NewNotFound("debt not found")
-	}
 	if n, _ := rows.RowsAffected(); n == 0 {
 		return Debt{}, errors.NewUnauthorized("User accepting is not the same as the lender")
+	}
+	if err != nil {
+		return Debt{}, errors.NewNotFound("debt not found")
 	}
 	updatedDebt, err := r.GetDebt(req.DebtId)
 	if err != nil {
